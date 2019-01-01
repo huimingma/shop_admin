76c0cc1dca57c0174a38d3fa0953496700de2026
mark <mark@qq.com>
2019-01-02 06:49:53 +0800

    查看

---
+++ shop-admin/src/components/login/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login">
+    <el-form :model="loginForm"
+             :rules="rules"
+             ref="loginForm"
+             label-width="100px"
+             class="demo-loginForm">
+
+      <el-form-item label="用户名"
+                    prop="name">
+        <el-input v-model="loginForm.username"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码"
+                    prop="name">
+        <el-input v-model="loginForm.password"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary"
+                   @click="submitForm('loginForm')">登录</el-button>
+        <el-button @click="resetForm('loginForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 5, max: 12, message: '用户名长度在5到12位之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 12, message: '密码长度在6到12位之间', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (!valid) {
+          return false
+        }
+        axios.post('http://localhost:8888/api/private/v1/login', this.loginForm).then(res => {
+          console.log(res)
+
+          if (res.data.meta.status === 200) {
+            localStorage.setItem('token', res.data.data.token)
+          }
+        })
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+<style>
+</style>
--- shop-admin/src/router/index.js
+++ shop-admin/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Login from '@/components/login/Login'
 
 // 安装插件
 Vue.use(Router)
 // 创建路由实例，并导出
 export default new Router({
   routes: [
-    {
-      path: '/',
-      component: HelloWorld
-    }
+    { path: '/login', component: Login, name: 'login' }
   ]
 })
